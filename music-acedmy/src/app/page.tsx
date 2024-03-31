cb458dc1f9a6fc654bfc4109c15b1b2d3e1d44e3
Dikshant441 <singhdikshant200@gmail.com>
2024-03-31 17:09:23 +0530

    build course and cotact component using Ui library

--- music-acedmy/src/app/page.tsx
+++ music-acedmy/src/app/page.tsx
@@ -1,5 +1,6 @@
 
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonial from "@/components/TestimonialCards";
@@ -16,6 +17,7 @@ export default function Home() {
       <MusicSchoolTestimonial/>
       <UpComingWebinars/>
       <Instructors/>
+      <Footer/>
     </main>
   );
 }
